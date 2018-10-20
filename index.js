e54a254a47cbef96569c62fd1d18cb8c3c24b317
Leigh Halliday <leighhalliday@gmail.com>
2018-10-20 12:28:52 -0400

    Setting port from env

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const fetch = require("node-fetch");
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 4000;
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
